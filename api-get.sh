b9822ccc1ded56293b3dcc4943ae82ad7d459978
juanman <juanman@localhost>
2017-09-20 01:30:48 -0700

    PC-2017.2.2 <juanman@localhost Overwrite remote https://github.com/developmex/zoom to local

--- api-get.sh
+++
@@ -1,8 +0,0 @@
-rm datos.json
-curl https://api.zoom.us/v1/recording/list \
-    -X POST \
-    -d api_key=g7MZBr7GT3CdFn07F1F2Bw \
-    -d api_secret=jEJouAV2JgKAEHLHe0eQogUL7yvKHHaDXY3y \
-    -d host_id=7pCPQl2tSC-S9QoSbZ3RHA   >  datos.json
-
-
--- djangozoom/videos/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.db import models
-
-
-class Webinar(models.Model):
-
-    meeting = models.CharField(max_length=30)
